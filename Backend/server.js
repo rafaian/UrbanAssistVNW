311e96018bdb27780870c0d10051a93fb9131da7
rafaian <rafaelserafimx@gmail.com>
2026-04-21 18:05:02 -0300

    conectando db com front, estrutura do app.jsx e estilização do app

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,11 @@
 const express = require("express")
 const app = express()
+const cors = require("cors")
+
+
+app.use(cors())
 app.use(express.json())
+
 const PORT = process.env.PORT || 3000
 const { criarBanco } = require("./database")
 
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,121 +1,147 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useEffect, useState } from "react"
+import axios from "axios"
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+const [abrigos, setAbrigos] = useState([])
+const [bairro, setBairro] = useState("")
 
-      <div className="ticks"></div>
+const [nome, setNome] = useState("")
+const [bairroNovo, setBairroNovo] = useState("")
+const [vagas, setVagas] = useState("")
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+async function carregarAbrigos() {
+
+const resposta = await axios.get(
+"http://localhost:3000/abrigos"
+)
+
+setAbrigos(resposta.data)
+
+}
+
+
+async function buscarPorBairro(){
+
+  const resposta = await axios.get(
+  `http://localhost:3000/buscar?bairro=${bairro}`
   )
+
+setAbrigos(resposta.data)
+
+}
+
+
+async function deletarAbrigo(id){
+
+  await axios.delete(
+  `http://localhost:3000/abrigos/${id}`
+  )
+
+carregarAbrigos()
+
+}
+
+
+async function cadastrarAbrigo(){
+
+  await axios.post(
+  "http://localhost:3000/abrigos",
+  {
+    nome: nome,
+    bairro: bairroNovo,
+    vagas: Number(vagas)
+  }
+  )
+
+carregarAbrigos()
+
+setNome("")
+setBairroNovo("")
+setVagas("")
+
+}
+
+
+useEffect(() => {
+carregarAbrigos()
+}, [])
+
+
+
+return (
+<div className="container">
+  <h1>UrbanAssist</h1>
+
+  <div className="bloco">
+    <h2>Cadastrar Abrigo</h2>
+  </div>
+  <div className="form-linha">
+    <input
+      placeholder="Nome"
+      value={nome}
+      onChange={(e)=>setNome(e.target.value)}
+      />
+
+    <input
+      placeholder="Bairro"
+      value={bairroNovo}
+      onChange={(e)=>setBairroNovo(e.target.value)}
+      />
+
+    <input
+      placeholder="Vagas"
+      value={vagas}
+      onChange={(e)=>setVagas(e.target.value)}
+      />
+    
+    <button onClick={cadastrarAbrigo}>
+      Salvar
+    </button>
+  </div>
+
+<div className="bloco">
+  <h2>Buscar por bairro</h2>
+</div>
+
+<input
+  type="text"
+  placeholder="Digite um bairro"
+  value={bairro}
+  onChange={(e)=>setBairro(e.target.value)}
+  />
+
+  <button onClick={buscarPorBairro}>
+    Buscar
+  </button>
+
+  <button onClick={carregarAbrigos}>
+    Mostrar todos
+  </button>
+
+
+<h2>Lista de Abrigos</h2>
+
+{abrigos.map((abrigo) => (
+
+  <div className= "card" key={abrigo.id}>
+
+    <h3>{abrigo.nome}</h3>
+    <p className="bairro">Bairro:{abrigo.bairro}</p>
+    <p className="vagas">Vagas:{abrigo.vagas}</p>
+
+    <button className="remover" onClick={() => removerAbrigo(abrigo.id)}>
+      Remover
+    </button>
+
+  </div>
+
+))}
+
+</div>
+)
+
 }
 
 export default App
